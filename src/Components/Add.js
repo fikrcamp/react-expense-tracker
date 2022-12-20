aa1a368e8271f3702b52fe931aff233b51609bee
zayid <sayidka843@gmail.com>
2022-12-20 23:44:10 +0600

    uploading the files react-expenses-tracker solution

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 // context
-function Add() {
+function Add(props) {
+  const [Amount,setAmount]= useState("")
+  const [Description,setDescription] = useState("")
+
   return (
     <div className="add-expense space">
-      <input className="expense-amount" type="number" placeholder="$" />
+      <input className="expense-amount" type="number" placeholder="$" onChange={(e)=> setAmount(e.target.value)}/>
       <input
+         onChange={(e)=> setDescription(e.target.value)}
         className="expense-input"
         type="text"
         placeholder="Add expense description"
       />
-      <button className="expense-button">
+      <button className="expense-button" onClick={()=>props.handleOnClick(Amount,Description)}>
         <FaPlus />
       </button>
     </div>
